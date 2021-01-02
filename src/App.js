128ae68eb780526e6a4646cb572d3c68ddd7074a
Coskun <copolat@yahoo.comcopolat git config --global user.name copolat>
2021-01-02 15:33:20 -0600

    Add event listener and handler

--- src/App.js
+++ src/App.js
@@ -4,15 +4,23 @@ class App extends React.Component {
   constructor(props){
     super(props);
     this.state = {
-      userName: 'Guest'
+      userName: 'Guest',
+      newUser: ''
     }
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(e) {
+    this.setState({
+      newUser: e.target.value,
+      userName: e.target.value
+    })
   }
   render() {
     return (
       <div>
         <h1>Hello {this.state.userName}</h1>
         <label htmlFor="user_name">Enter your name: </label> 
-        <input id="user_name" type="text"/>
+        <input id="user_name" type="text" onChange={this.handleChange} value={this.state.newUser}/>
         </div>
     )
   }
