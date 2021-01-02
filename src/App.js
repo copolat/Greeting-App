263bfbbced6752f2324b84f516844416b0da3f2c
Coskun <copolat@yahoo.comcopolat git config --global user.name copolat>
2021-01-02 14:47:59 -0600

    Convert functional component into class component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-function Header() {
-  return (
-    <div>
-      <h1>Hello World</h1>
-      <label htmlFor="user_name">Enter your name</label>
-      <input id="user_name" type="text"/>
-      </div>
-  )
+import React from 'react'
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello World</h1>
+        <label htmlFor="user_name">Enter your name: </label> 
+        <input id="user_name" type="text"/>
+        </div>
+    )
+  }
 }
-export default Header;
+export default App;
